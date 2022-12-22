0d11d9f407b9cde211bc9318a8826b917e517702
JordiCar-design <jordicll@gmail.com>
2022-12-22 18:27:40 +0100

    1er commit

---
+++ codi/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    Hola
+  </v-container>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  data(){
+    return{
+      todos:null
+    }
+  },
+  mounted(){
+    axios.get('https://dummyjson.com/todos')
+    .then(resposta=>{
+      console.log(resposta)
+      this.todos = resposta.todos
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+    /*
+    .catch(function(err){
+      console.log(err)
+    })*/
+  }
+  
+}
+</script>
